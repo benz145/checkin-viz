285d64cd02768390c2369e7499fd793ea3788dad
tcrez <trescenzi@gmail.com>
2025-10-21 00:31:07 +0000

    chore: update calc formatting

--- src/slash_commands/calc.py
+++ src/slash_commands/calc.py
@@ -24,10 +24,10 @@ class Modal(discord.ui.Modal):
         timeToNextTier = self.time_for_next_tier(timeTier) - time
 
         embed = discord.Embed(title="Tier Results")
-        embed.add_field(name="Calories tier:", value=calTier, inline=False)
-        embed.add_field(name="Time tier:", value=timeTier, inline=False)
-        embed.add_field(name="Calories to next tier:", value=calToNextTier, inline=False)
-        embed.add_field(name="Time to next tier:", value=timeToNextTier, inline=False)
+        embed.add_field(name="Calories tier:", value=f"T{calTier}", inline=False)
+        embed.add_field(name="Time tier:", value=f"T{timeTier}", inline=False)
+        embed.add_field(name="Calories to next tier:", value=f"{calToNextTier} calories", inline=False)
+        embed.add_field(name="Time to next tier:", value=f"{timeToNextTier} minutes", inline=False)
 
         await interaction.response.send_message(embeds=[embed], ephemeral=True)
 
@@ -40,7 +40,7 @@ class Modal(discord.ui.Modal):
         return math.floor(15 * (nextTier + 1))
 
     def tier_for_calories(self, bmr, calories):
-        return math.floor((20*(calories + 1))/bmr - 2)
+        return max(0, math.floor((20*(calories + 1))/bmr - 2))
 
     def tier_for_time(self, time):
-        return math.floor(time/15-1)
+        return max(0, math.floor(time/15-1))
