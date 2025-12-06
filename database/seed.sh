65284ecbc04bea7afcaff890ff37beade3beefc8
Thomas Crescenzi <trescenzi@gmail.com>
2025-12-06 09:39:46 -0500

    fix: seed current challenge too

--- database/seed.sh
+++ database/seed.sh
@@ -6,7 +6,11 @@ echo "------------ Creating Tables -------------"
 # -h postgres works because the service's name is postgres
 # and we're assuming this runs in docker
 PGPASSWORD=password pg_restore -d projects -h postgres -U postgres -cCO tables.dump
+
 echo "------- Beginning Database Seeding -------"
+echo "------------------------------------------"
+
+echo "----- Copying Challenge 23 as a Base -----"
 echo "---------- Copying Challengers -----------"
 psql $PROD_DB_CONNECT_STRING -c "copy (select * from challengers) to stdout" | \
   psql $LOCAL_DB_CONNECT_STRING -c "copy challengers from stdin"
@@ -18,7 +22,12 @@ psql $PROD_DB_CONNECT_STRING -c "copy (select * from challenge_weeks where chall
   psql $LOCAL_DB_CONNECT_STRING -c "copy challenge_weeks from stdin"
 echo "------------ Copying Checkins ------------"
 # we have to be extra explicit here because of the two automatic columns
-psql $PROD_DB_CONNECT_STRING -c "copy (select name, time, tier, day_of_week, text, challenge_week_id, checkins.id, challenger, tz from checkins join challenge_weeks ON challenge_weeks.id = checkins.challenge_week_id where challenge_weeks.challenge_id = 23) to stdout" | \
+psql $PROD_DB_CONNECT_STRING -c \
+"copy (
+  select name, time, tier, day_of_week, text, challenge_week_id, checkins.id, challenger, tz 
+  from checkins
+  join challenge_weeks ON challenge_weeks.id = checkins.challenge_week_id
+  where challenge_weeks.challenge_id = 23) to stdout" | \
   psql $LOCAL_DB_CONNECT_STRING -c "copy checkins from stdin"
 # mulligans depends on checkins
 echo "------ Copying Challenger Challenges -----"
@@ -27,6 +36,48 @@ psql $PROD_DB_CONNECT_STRING -c "copy (select * from challenger_challenges where
 echo "------------ Copying Medals -------------"
 psql $PROD_DB_CONNECT_STRING -c "copy (select * from medals where challenge_id = 23) to stdout" | \
   psql $LOCAL_DB_CONNECT_STRING -c "copy medals from stdin"
+echo "------------------------------------------"
+
+echo "------- Copying Current Challenge --------"
+echo "----------- Copying Challenge ------------"
+psql $PROD_DB_CONNECT_STRING -c \
+"copy (select * from challenges c
+    where (current_timestamp at time zone 'America/New_York')::date >= c.start
+    and (current_timestamp at time zone 'America/New_York')::date <= c.end) to stdout" | \
+  psql $LOCAL_DB_CONNECT_STRING -c "copy challenges from stdin"
+echo "--------- Copying Challenge Weeks --------"
+psql $PROD_DB_CONNECT_STRING -c \
+  "copy (select cw.* from challenge_weeks cw join challenges c on c.id = cw.challenge_id
+    where (current_timestamp at time zone 'America/New_York')::date >= c.start
+    and (current_timestamp at time zone 'America/New_York')::date <= c.end) to stdout" | \
+  psql $LOCAL_DB_CONNECT_STRING -c "copy challenge_weeks from stdin"
+echo "------------ Copying Checkins ------------"
+# we have to be extra explicit here because of the two automatic columns
+psql $PROD_DB_CONNECT_STRING -c \
+"copy (select ch.name, ch.time, ch.tier, ch.day_of_week, ch.text, ch.challenge_week_id, ch.id, ch.challenger, ch.tz 
+  from checkins ch
+  join challenge_weeks cw ON cw.id = ch.challenge_week_id
+  join challenges c on cw.challenge_id = c.id
+  where (current_timestamp at time zone 'America/New_York')::date >= c.start
+    and (current_timestamp at time zone 'America/New_York')::date <= c.end) to stdout" | \
+  psql $LOCAL_DB_CONNECT_STRING -c "copy checkins from stdin"
+# mulligans depends on checkins
+echo "------ Copying Challenger Challenges -----"
+psql $PROD_DB_CONNECT_STRING -c \
+  "copy (select cc.* from challenger_challenges cc
+  join challenges c on c.id = cc.challenge_id
+  where (current_timestamp at time zone 'America/New_York')::date >= c.start
+    and (current_timestamp at time zone 'America/New_York')::date <= c.end) to stdout" | \
+  psql $LOCAL_DB_CONNECT_STRING -c "copy challenger_challenges from stdin"
+echo "------------ Copying Medals -------------"
+psql $PROD_DB_CONNECT_STRING -c \
+  "copy (select * from medals
+  join challenges c on c.id = challenge_id
+  where (current_timestamp at time zone 'America/New_York')::date >= c.start
+    and (current_timestamp at time zone 'America/New_York')::date <= c.end) to stdout" | \
+  psql $LOCAL_DB_CONNECT_STRING -c "copy medals from stdin"
+echo "-----------------------------------------"
+
 echo "----------- Copying Functions -----------"
 psql $PROD_DB_CONNECT_STRING -A -t -c "SELECT pg_get_functiondef(oid) || ';' FROM pg_proc where proname like '%score%'" | \
   psql $LOCAL_DB_CONNECT_STRING
