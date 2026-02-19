2fba52c508661fc14ae7401b385a5136c05448dc
Thomas Crescenzi <trescenzi@gmail.com>
2026-02-18 22:24:22 -0500

    fix: bye weeks are never green weeks

--- src/green.py
+++ src/green.py
@@ -8,21 +8,33 @@ def number_of_non_green_weeks_before_this_one(challenge_id):
     sql = """
   select count(*) from challenge_weeks
        where
-       challenge_id = 25
+       challenge_id = %s
        /* the end is after the last green week or the first week */
        and "end" > coalesce(
-           (select "end" from challenge_weeks where green = true and challenge_id = 25 order by "end" desc limit 1),
-           (select "end" from challenge_weeks where challenge_id = 25 order by "end" asc limit 1)
+           (select "end" from challenge_weeks where green = true and challenge_id = %s order by "end" desc limit 1),
+           (select "end" from challenge_weeks where challenge_id = %s order by "end" asc limit 1)
         )
        /* the end is before now */
        and "end" < current_date
   """
-    return fetchone(sql, [challenge_id]).count
+    return fetchone(sql, [challenge_id, challenge_id, challenge_id]).count
 
 
 def determine_if_green():
     challenge_week = get_current_challenge_week()
+
+    # If this is a bye week, set green to false and return
+    if challenge_week.bye_week:
+        def set_green(conn, cur):
+            cur.execute(
+                "update challenge_weeks set green = false where id = %s",
+                [challenge_week.id],
+            )
+        with_psycopg(set_green)
+        return False
+
     if challenge_week.green is None:
+        
         num_non_green = number_of_non_green_weeks_before_this_one(
             challenge_week.challenge_id
         )
