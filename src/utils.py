915f2a459d538da855ca03554cf7e0ec2463a906
tcrez <trescenzi@gmail.com>
2026-04-24 00:16:03 +0000

    chore: increase specificity for checkins

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,9 @@ import re
 
 
 def get_tier(message):
-    match = re.match(".*(t\\d+).*", message.lower())
-    if match is not None:
-        return match.group(1).upper()
+    m = message.lower()
+    if "checkin" in m or "check-in" in m:
+        match = re.match(".*(t\\d+).*", m)
+        if match is not None:
+            return match.group(1).upper()
     return "unknown"
