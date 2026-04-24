c1daa47bfd7183947c9712a06a621730733a8c96
tcrez <trescenzi@gmail.com>
2026-04-24 22:27:01 +0000

    fix: add check in

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import re
 
 def get_tier(message):
     m = message.lower()
-    if "checkin" in m or "check-in" in m:
+    if "checkin" in m or "check-in" in m or "check in" in m:
         match = re.match(".*(t\\d+).*", m)
         if match is not None:
             return match.group(1).upper()
